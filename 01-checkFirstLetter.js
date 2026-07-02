a3066e7a410aac866db4ae0e286f658a028a49fe
Md. Emon Hossain <eeemonhossain322@gmail.com>
2026-07-02 21:50:14 +0600

    Update 01-checkFirstLetter.js

--- 01-checkFirstLetter.js
+++ 01-checkFirstLetter.js
@@ -1,8 +1,17 @@
+/*
+ * File Name: 01-checkFirstLetter.js
+ * Description: দুটি শব্দের প্রথম অক্ষর চেক করে। যদি দুটো শব্দ একই অক্ষর দিয়ে শুরু হয়, 
+ *              তাহলে একটি মেসেজ রিটার্ন করে, নাহলে "No match." রিটার্ন করে।
+ * Author: Emon 
+ * Date: 6.2.26
+ */
+
+
 function similarWord(first, second){
   let firstWord = first[0];
   let secondWord = second[0];
   if (firstWord === secondWord){
-    return `Wow, ${first} and ${second} both start with the same letter!`
+    return `Wow, ${first} and ${second} both start with the same letter!`;
   }
   else{
     return "No match.";

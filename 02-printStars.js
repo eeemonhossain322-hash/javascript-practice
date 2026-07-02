97df54c1cb2af5ec3a66b5ef63b0a6bbe8371a7e
Md. Emon Hossain <eeemonhossain322@gmail.com>
2026-07-02 21:48:09 +0600

    Update 02-printStars.js

--- 02-printStars.js
+++ 02-printStars.js
@@ -1,3 +1,11 @@
+/*
+ * File Name: 02-printStars.js
+ * Description: একটি নির্দিষ্ট সংখ্যা অনুযায়ী স্টার (*) প্রিন্ট করে এবং মোট কয়টা স্টার সেটা ব্র্যাকেটে দেখায়।
+ * Author: Emon
+ * Date: 7.2.26
+ */
+
+
 function printStars(num){
   let repeatNum = "*".repeat(num);
   return `${repeatNum} (${num} Stars)`
